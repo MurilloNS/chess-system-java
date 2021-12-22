633c8ebcd5120c9b283bb42b70fe08bc7cf0848e
Murillo Nonato <murillo.murillo2001@gmail.com>
2021-12-21 23:01:21 -0300

    Starting to implement Board and Piece

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -3,10 +3,12 @@ package boardGame;
 public class Position {
 	private int row;
 	private int column;
+	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
+	
 	public int getRow() {
 		return row;
 	}
